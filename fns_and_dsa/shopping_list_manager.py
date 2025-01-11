36098d3ad2a3b8f299229079d9405ef91f124d6a
mujib-sultan <ibnsultan888@gmail.com>
2025-01-11 08:40:49 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -6,7 +6,7 @@ def display_menu():
     print("4. Exit")
 
 def main():
-    shopping_list = [ ]
+    shopping_list = []
     while True:
         display_menu()
         choice = input("Enter your choice: ")
